140e49df33c555df38eeaab1e627c304c31e3178
SebLeyr <sebastien.leyrissoux@gmail.com>
2022-01-10 11:54:46 +0100

    correction querySelectors

--- userBase.js
+++ userBase.js
@@ -1,20 +1,19 @@
 var tab=[];
 var cpt=0;
-const result = document.querySelector("resultat");
-const nbrCompte = document.querySelector("nbrCompte");
-const error = document.querySelector("error");
+const result = document.querySelector("#resultat");
+const nbrCompte = document.querySelector("#nbrCompte");
+const error = document.querySelector("#error");
 
 function ajouter() {
-    const name = document.querySelector("name");
-    const firstName = document.querySelector("firstName");
-    const login = document.querySelector("login");
-    const mdp = document.querySelector("mdp");
-    const user = [name, firstName, login, mdp];
+    const name = document.querySelector("#name").value;
+    const firstName = document.querySelector("#firstName").value;
+    const login = document.querySelector("#login").value;
+    const mdp = document.querySelector("#mdp").value;
 
     if(name != "" && firstName != "" && login != "" && mdp != ""){
-        tab[cpt] = user;
+        tab[cpt] = [name, firstName, login, mdp];
+        result.innerHTML = `Nom : ${tab[cpt][0]}, Prénom : ${tab[cpt][1]}, Login : ${tab[cpt][2]}, Mot de passe : ${tab[cpt][3]}`;
         cpt ++;
-        result.innerHTML = `Nom : ${name} Prénom : ${firstName} Login : ${login} Mot de passe : ${mdp}`;
         nbrCompte.innerHTML = `Il y a ${cpt} comptes utilisateur`;
     } else {
         error.innerHTML = "Un champ est vide, veuillez le remplir";
