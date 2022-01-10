ec43f74b9e7d8f4d7b4f977f7e0ffa3233b99248
SebLeyr <sebastien.leyrissoux@gmail.com>
2022-01-10 11:29:24 +0100

    correction ligne user

--- userBase.js
+++ userBase.js
@@ -9,12 +9,13 @@ function ajouter() {
     const firstName = document.querySelector("firstName");
     const login = document.querySelector("login");
     const mdp = document.querySelector("mdp");
-    const user = [name, firstName, login, mdp, user];$
+    const user = [name, firstName, login, mdp];
 
     if(name != "" && firstName != "" && login != "" && mdp != ""){
-        tab[nbrCompte] = user;
-        nbrCompte ++;
-        result.innerHTML (`Nom : ${name} Prénom : ${firstName} Login : ${login} Mot de passe : ${mdp}`)
+        tab[cpt] = user;
+        cpt ++;
+        result.innerHTML (`Nom : ${name} Prénom : ${firstName} Login : ${login} Mot de passe : ${mdp}`);
+        nbrCompte.innerHTML (`Il y a ${cpt} comptes utilisateur`)
     } else {
         error.innerHTML("Un champ est vide, veuillez le remplir")
     }
